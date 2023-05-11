d88334b29bdd8fb5f22b61a353020e39003dcb58
Isabelle Reny <isabelleribaldo@hotmail.com>
2023-05-11 01:45:49 -0300

    Initial commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Sprite[] tileGraphics;
+
+    private void Start(){
+        rend = GetComponent<SpriteRenderer>();
+        int randTile =Random.Range(0, tileGraphics.Length);
+        rend.sprite = tileGraphics [randTile];
+    }
+}
